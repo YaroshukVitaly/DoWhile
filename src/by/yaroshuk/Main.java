2252643222665aa64490641fb4cba2a1b01c2c88
Vitalya <yaroshukvi@gmail.com>
2018-08-18 21:18:21 +0300

    Initial commit

---
+++ src/by/yaroshuk/Main.java
@@ -0,0 +1,16 @@
+package by.yaroshuk;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+            if (i >= 10){
+                break;
+            }
+        } while (true);
+
+    }
+}
